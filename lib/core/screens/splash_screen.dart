3550722f64a27e1563a9a075fd58ce56f383bce7
dadzi <dadziemikke2@gmail.com>
2021-10-28 19:37:41 +0000

    Moved splash_screen.dart to the screens directory

---
+++ lib/core/screens/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:socially/core/screens/screens.dart';
+import 'package:socially/feautures/authentication/presentation/bloc/auth/auth_bloc.dart';
+import 'package:socially/feautures/authentication/presentation/pages/login_screen.dart';
+
+class SplashScreen extends StatelessWidget {
+  static const String routeName = '/splash';
+  const SplashScreen({Key? key}) : super(key: key);
+
+  static Route route() {
+    return MaterialPageRoute(
+      settings: const RouteSettings(name: routeName),
+      builder: (_) => SplashScreen(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state.satus == AuthStatus.unauthenticated) {
+            //Go to login screen
+            Navigator.of(context).pushNamed(LoginScreen.routeName);
+          } else if (state.satus == AuthStatus.authenticated) {
+            // Go to home screen
+            Navigator.of(context).pushNamed(BottomNavBar.routeName);
+          }
+          print(state);
+        },
+        child: Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
