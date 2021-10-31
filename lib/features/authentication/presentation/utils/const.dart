4efb7bd391cf07a4d3508d6b2e02d98e907944d2
dadzi <dadziemikke2@gmail.com>
2021-10-31 11:15:52 +0000

    Improved UI code

--- lib/features/authentication/presentation/utils/const.dart
+++ lib/features/authentication/presentation/utils/const.dart
@@ -3,12 +3,11 @@ import 'package:google_fonts/google_fonts.dart';
 
 final textFormFieldDecoration = InputDecoration(
   hintText: '',
-  fillColor: Colors.white,
   filled: true,
   hintStyle: GoogleFonts.lato(
     fontSize: 13,
     fontWeight: FontWeight.w400,
-    color: Color.fromRGBO(136, 147, 164, 1),
+    color: Colors.grey.shade500,
   ),
   errorStyle: GoogleFonts.lato(
     fontSize: 13,
@@ -17,16 +16,18 @@ final textFormFieldDecoration = InputDecoration(
   ),
   errorBorder: OutlineInputBorder(
     borderRadius: BorderRadius.circular(10),
-    borderSide: new BorderSide(color: Colors.red),
+    borderSide: const BorderSide(color: Colors.transparent),
   ),
   focusedBorder: OutlineInputBorder(
     borderRadius: BorderRadius.circular(10),
-    borderSide: BorderSide(
-      color: Color.fromRGBO(19, 119, 188, 1),
-    ),
+    borderSide: const BorderSide(color: Colors.transparent),
   ),
   enabledBorder: OutlineInputBorder(
     borderRadius: BorderRadius.circular(10),
-    borderSide: BorderSide(color: Colors.grey.shade200),
+    borderSide: const BorderSide(color: Colors.transparent),
+  ),
+  disabledBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10),
+    borderSide: const BorderSide(color: Colors.transparent),
   ),
 );
