cc5465abc57ff686eb9ebc8d8764808265fec0fd
dadzi <dadziemikke2@gmail.com>
2021-10-31 23:47:24 +0000

    Connected login screen to login cubit

--- lib/features/authentication/presentation/pages/login_screen.dart
+++ lib/features/authentication/presentation/pages/login_screen.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:socially/features/authentication/data/repositories/auth_repository.dart';
+import 'package:socially/features/authentication/presentation/cubit/login_cubit.dart';
 import 'package:socially/features/authentication/presentation/extension/form_extension.dart';
 import 'package:socially/features/authentication/presentation/utils/const.dart';
 import 'package:socially/features/authentication/presentation/widgets/custom_button.dart';
@@ -12,7 +16,10 @@ class LoginScreen extends StatefulWidget {
     return PageRouteBuilder(
         settings: const RouteSettings(name: routeName),
         transitionDuration: const Duration(seconds: 0),
-        pageBuilder: (_, __, ___) => LoginScreen());
+        pageBuilder: (context, _, __) => BlocProvider<LoginCubit>(
+            create: (_) =>
+                LoginCubit(authRepository: context.read<AuthRepository>()),
+            child: LoginScreen()));
   }
 
   @override
@@ -38,151 +45,187 @@ class _LoginScreenState extends State<LoginScreen> {
       onWillPop: () async => false,
       child: GestureDetector(
         onTap: () => FocusScope.of(context).unfocus(),
-        child: Scaffold(
-          // resizeToAvoidBottomInset: false,
-          body: SafeArea(
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 15, right: 15),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Socially',
-                            style: GoogleFonts.aBeeZee(
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
-                              color: Theme.of(context).focusColor,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                          const SizedBox(height: 30),
-                          SizedBox(
-                            height: 55,
-                            child: TextFormField(
-                              controller: _email,
-                              keyboardType: TextInputType.emailAddress,
-                              onFieldSubmitted: (_) => node.unfocus(),
-                              decoration: textFormFieldDecoration.copyWith(
-                                hintText: 'Username or email',
-                                fillColor: Theme.of(context).hintColor,
-                              ),
-                              style: GoogleFonts.lato(
-                                fontSize: 17,
-                                fontWeight: FontWeight.w400,
-                                color: Theme.of(context).focusColor,
-                              ),
-                              onChanged: (value) => print(value),
-                              validator: (String? v) {
-                                if (v!.isValidEmail) {
-                                  return null;
-                                } else {
-                                  return 'Please enter a valid email';
-                                }
-                              },
-                            ),
-                          ),
-                          const SizedBox(height: 12),
-                          SizedBox(
-                            height: 55,
-                            child: TextFormField(
-                              controller: _password,
-                              obscureText: !_passwordVisible!,
-                              keyboardType: TextInputType.visiblePassword,
-                              onFieldSubmitted: (_) => node.unfocus(),
-                              decoration: textFormFieldDecoration.copyWith(
-                                hintText: 'Password',
-                                fillColor: Theme.of(context).hintColor,
-                                suffixIcon: IconButton(
-                                  icon: Icon(
-                                    _passwordVisible!
-                                        ? Icons.visibility
-                                        : Icons.visibility_off,
-                                    color: Theme.of(context).focusColor,
-                                  ),
-                                  splashColor: Colors.transparent,
-                                  highlightColor: Colors.transparent,
-                                  onPressed: () {
-                                    setState(() {
-                                      _passwordVisible = !_passwordVisible!;
-                                    });
-                                  },
-                                ),
-                              ),
-                              style: GoogleFonts.lato(
-                                fontSize: 17,
-                                fontWeight: FontWeight.w400,
-                                color: Theme.of(context).focusColor,
-                              ),
-                              onChanged: (value) => print(value),
-                              validator: (String? v) {
-                                if (v!.isValidPassword) {
-                                  return null;
-                                } else {
-                                  return 'Must be at least 8 characters';
-                                }
-                              },
-                            ),
-                          ),
-                          const SizedBox(height: 35),
-                          CustomButton(
-                            title: 'Log In',
-                            bordersideColor: Colors.blue,
-                            textColor: Colors.white,
-                            backgroundColor: Colors.blue,
-                            onPress: () {},
-                          ),
-                        ],
-                      ),
-                    ),
+        child: BlocConsumer<LoginCubit, LoginState>(
+          listener: (context, state) {
+            if (state.status == LoginStatus.error) {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  backgroundColor: Theme.of(context).splashColor,
+                  title: Text(
+                    'Error',
+                    style: GoogleFonts.lato(
+                        color: Theme.of(context).focusColor,
+                        fontWeight: FontWeight.bold),
                   ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(bottom: 20, left: 20, right: 20),
-                    child: Column(
-                      children: [
-                        Divider(color: Theme.of(context).dividerColor),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 15),
-                          child: Row(
+                  content: Text(
+                    state.failure.message,
+                    style:
+                        GoogleFonts.lato(color: Theme.of(context).focusColor),
+                  ),
+                ),
+              );
+            }
+          },
+          builder: (context, state) {
+            return Scaffold(
+              // resizeToAvoidBottomInset: false,
+              body: SafeArea(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 15, right: 15),
+                          child: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Text(
-                                'Don\'t have an account?',
-                                style: GoogleFonts.roboto(
-                                  fontSize: 13,
-                                  fontWeight: FontWeight.w400,
+                                'Socially',
+                                style: GoogleFonts.aBeeZee(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
                                   color: Theme.of(context).focusColor,
                                 ),
+                                textAlign: TextAlign.center,
                               ),
-                              const SizedBox(width: 5),
-                              GestureDetector(
-                                onTap: () {},
-                                child: Text(
-                                  'Sign Up.',
-                                  style: GoogleFonts.roboto(
-                                    fontSize: 13,
-                                    fontWeight: FontWeight.w700,
-                                    color: Color.fromRGBO(41, 170, 225, 1),
+                              const SizedBox(height: 30),
+                              SizedBox(
+                                height: 55,
+                                child: TextFormField(
+                                  controller: _email,
+                                  keyboardType: TextInputType.emailAddress,
+                                  onFieldSubmitted: (_) => node.unfocus(),
+                                  decoration: textFormFieldDecoration.copyWith(
+                                    hintText: 'Username or email',
+                                    fillColor: Theme.of(context).hintColor,
                                   ),
+                                  style: GoogleFonts.lato(
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.w400,
+                                    color: Theme.of(context).focusColor,
+                                  ),
+                                  onChanged: (value) => context
+                                      .read<LoginCubit>()
+                                      .emailChanged(value),
+                                  validator: (String? v) {
+                                    if (v!.isValidEmail) {
+                                      return null;
+                                    } else {
+                                      return 'Please enter a valid email';
+                                    }
+                                  },
                                 ),
+                              ),
+                              const SizedBox(height: 15),
+                              SizedBox(
+                                height: 55,
+                                child: TextFormField(
+                                  controller: _password,
+                                  obscureText: !_passwordVisible!,
+                                  keyboardType: TextInputType.visiblePassword,
+                                  onFieldSubmitted: (_) => node.unfocus(),
+                                  decoration: textFormFieldDecoration.copyWith(
+                                    hintText: 'Password',
+                                    fillColor: Theme.of(context).hintColor,
+                                    suffixIcon: IconButton(
+                                      icon: Icon(
+                                        _passwordVisible!
+                                            ? Icons.visibility
+                                            : Icons.visibility_off,
+                                        color: Theme.of(context).focusColor,
+                                      ),
+                                      splashColor: Colors.transparent,
+                                      highlightColor: Colors.transparent,
+                                      onPressed: () {
+                                        setState(() {
+                                          _passwordVisible = !_passwordVisible!;
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                  style: GoogleFonts.lato(
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.w400,
+                                    color: Theme.of(context).focusColor,
+                                  ),
+                                  onChanged: (value) => context
+                                      .read<LoginCubit>()
+                                      .passwordChanged(value),
+                                  validator: (String? v) {
+                                    if (v!.isValidPassword) {
+                                      return null;
+                                    } else {
+                                      return 'Must be at least 8 characters';
+                                    }
+                                  },
+                                ),
+                              ),
+                              const SizedBox(height: 35),
+                              CustomButton(
+                                title: 'Log In',
+                                bordersideColor: Colors.blue,
+                                textColor: Colors.white,
+                                backgroundColor: Colors.blue,
+                                onPress: () => _submitForm(context,
+                                    state.status == LoginStatus.submitting),
                               ),
                             ],
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            bottom: 20, left: 20, right: 20),
+                        child: Column(
+                          children: [
+                            Divider(color: Theme.of(context).dividerColor),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 15),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Don\'t have an account?',
+                                    style: GoogleFonts.roboto(
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w400,
+                                      color: Theme.of(context).focusColor,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 5),
+                                  GestureDetector(
+                                    onTap: () {},
+                                    child: Text(
+                                      'Sign Up.',
+                                      style: GoogleFonts.roboto(
+                                        fontSize: 13,
+                                        fontWeight: FontWeight.w700,
+                                        color: Color.fromRGBO(41, 170, 225, 1),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
-            ),
-          ),
+            );
+          },
         ),
       ),
     );
+  }
+
+  void _submitForm(BuildContext context, bool isSubmitting) {
+    if (_formKey.currentState!.validate() && !isSubmitting) {
+      context.read<LoginCubit>().logInWithCredentials();
+    }
   }
 }
