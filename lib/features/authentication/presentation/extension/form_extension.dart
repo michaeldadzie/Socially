b3def2f94a95787d25343da1607a9def0357e39f
dadzi <dadziemikke2@gmail.com>
2021-11-02 23:07:03 +0000

    Added regex to check for valid username

--- lib/features/authentication/presentation/extension/form_extension.dart
+++ lib/features/authentication/presentation/extension/form_extension.dart
@@ -1,4 +1,10 @@
 extension ExtString on String {
+  bool get isValidUserName {
+    final emailRegExp = RegExp(
+        r"^[a-zA-Z0-9](_(?!(\.|_))|\.(?!(_|\.))|[a-zA-Z0-9]){6,18}[a-zA-Z0-9]$");
+    return emailRegExp.hasMatch(this);
+  }
+
   bool get isValidEmail {
     final emailRegExp = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
     return emailRegExp.hasMatch(this);
