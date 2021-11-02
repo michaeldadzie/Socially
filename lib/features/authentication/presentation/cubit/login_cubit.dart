03f7cb4bf667112af237d48553a0fbc44778ca03
dadzi <dadziemikke2@gmail.com>
2021-11-02 23:07:29 +0000

    Refactored code

--- lib/features/authentication/presentation/cubit/login_cubit.dart
+++ lib/features/authentication/presentation/cubit/login_cubit.dart
@@ -1,6 +1,5 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:socially/features/authentication/data/models/failure_model.dart';
 import 'package:socially/features/authentication/data/repositories/auth_repository.dart';
 
