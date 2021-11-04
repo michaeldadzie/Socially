befc86a185ef9aea59c501981d9b2c7b0ad17c46
dadzi <dadziemikke2@gmail.com>
2021-11-04 22:30:49 +0000

    Added bottom nav bar cubit

---
+++ lib/core/nav/cubit/bottom_nav_bar_cubit.dart
@@ -0,0 +1,16 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:socially/core/nav/enums/bottom_nav_item.dart';
+
+part 'bottom_nav_bar_state.dart';
+
+class BottomNavBarCubit extends Cubit<BottomNavBarState> {
+  BottomNavBarCubit()
+      : super(BottomNavBarState(selectedItem: BottomNavItem.feed));
+
+  void updateSelectedItem(BottomNavItem item) {
+    if (item != state.selectedItem) {
+      emit(BottomNavBarState(selectedItem: item));
+    }
+  }
+}
