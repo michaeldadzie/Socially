a83d8904a71fe43d16f0c8f6a8549509248f0a92
dadzi <dadziemikke2@gmail.com>
2021-11-11 11:40:40 +0000

    Added base user repository

---
+++ lib/features/profile/data/repositories/base_user_repository.dart
@@ -0,0 +1,6 @@
+import 'package:socially/features/profile/data/models/user_model.dart';
+
+abstract class BaseUserRepository {
+  Future<User> getUserWithId({required String userId});
+  Future<void> updateUser({required User user});
+}
