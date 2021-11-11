4293bcf28f880c9a74c9b0b9a3e5cf0f74580144
dadzi <dadziemikke2@gmail.com>
2021-11-11 11:41:19 +0000

    Added profile bloc

---
+++ lib/features/profile/presentation/bloc/profile_bloc.dart
@@ -0,0 +1,50 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:socially/features/authentication/data/models/failure_model.dart';
+import 'package:socially/features/authentication/presentation/bloc/auth/auth_bloc.dart';
+import 'package:socially/features/profile/data/models/user_model.dart';
+import 'package:socially/features/profile/data/repositories/user_repository.dart';
+
+part 'profile_event.dart';
+part 'profile_state.dart';
+
+class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
+  final UserRepository _userRepository;
+  final AuthBloc _authBloc;
+
+  ProfileBloc({
+    required UserRepository userRepository,
+    required AuthBloc authBloc,
+  })  : _userRepository = userRepository,
+        _authBloc = authBloc,
+        super(ProfileState.initial());
+
+  @override
+  Stream<ProfileState> mapEventToState(ProfileEvent event) async* {
+    if (event is ProfileLoadUser) {
+      yield* _mapProfileLoadUserToState(event);
+    }
+  }
+
+// void _mapEventToState(ProfileEvent event, Emitter<>)
+
+  Stream<ProfileState> _mapProfileLoadUserToState(
+    ProfileLoadUser event,
+  ) async* {
+    yield state.copyWith(status: ProfileStatus.loading);
+    try {
+      final user = await _userRepository.getUserWithId(userId: event.userId);
+      final isCurrentUser = _authBloc.state.user?.uid == event.userId;
+      yield state.copyWith(
+        user: user,
+        isCurrentUser: isCurrentUser,
+        status: ProfileStatus.loaded,
+      );
+    } catch (err) {
+      yield state.copyWith(
+        status: ProfileStatus.error,
+        failure: const Failure(message: 'We were unable to load this profile'),
+      );
+    }
+  }
+}
