8e1f14a79b466fe6e1488634702e7d0d07a48516
dadzi <dadziemikke2@gmail.com>
2021-11-11 11:41:00 +0000

    Added user repository

---
+++ lib/features/profile/data/repositories/user_repository.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:socially/features/authentication/data/config/paths.dart';
+import 'package:socially/features/profile/data/models/user_model.dart';
+import 'package:socially/features/profile/data/repositories/base_user_repository.dart';
+
+class UserRepository extends BaseUserRepository {
+  final FirebaseFirestore _firebaseFirestore;
+
+  UserRepository({FirebaseFirestore? firebaseFirestore})
+      : _firebaseFirestore = firebaseFirestore ?? FirebaseFirestore.instance;
+
+  @override
+  Future<User> getUserWithId({required String? userId}) async {
+    final doc =
+        await _firebaseFirestore.collection(Paths.users).doc(userId).get();
+    return doc.exists ? User.fromDocument(doc) : User.empty;
+  }
+
+  @override
+  Future<void> updateUser({required User? user}) async {
+    await _firebaseFirestore
+        .collection(Paths.users)
+        .doc(user!.id)
+        .update(user.toDocument());
+  }
+}
