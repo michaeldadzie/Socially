d3ca1a7f9b51079975c5c496ea4f939fc2becb98
dadzi <dadziemikke2@gmail.com>
2021-11-15 19:55:15 +0000

    Removed base user repository

--- lib/features/profile/data/repositories/base_user_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:socially/features/profile/data/models/user_model.dart';
-
-abstract class BaseUserRepository {
-  Future<User> getUserWithId({required String userId});
-  Future<void> updateUser({required User user});
-}
