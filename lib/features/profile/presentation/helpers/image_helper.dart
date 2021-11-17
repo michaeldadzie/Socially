d1f7a3b27a1cce49c6bc346d70dabae811fa46a0
dadzi <dadziemikke2@gmail.com>
2021-11-17 20:09:37 +0000

    Added image helper to crop images picked from gallery

---
+++ lib/features/profile/presentation/helpers/image_helper.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImageHelper {
+  static Future<File?> pickedImageFromGallery({
+    required BuildContext context,
+    required CropStyle cropStyle,
+    required title,
+  }) async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      final croppedFile = await ImageCropper.cropImage(
+        sourcePath: pickedFile.path,
+        cropStyle: cropStyle,
+        androidUiSettings: AndroidUiSettings(
+          toolbarTitle: title,
+          toolbarColor: Theme.of(context).primaryColor,
+          toolbarWidgetColor: Theme.of(context).focusColor,
+          backgroundColor: Theme.of(context).primaryColor,
+          hideBottomControls: false,
+          initAspectRatio: CropAspectRatioPreset.original,
+          lockAspectRatio: false,
+        ),
+        iosUiSettings: const IOSUiSettings(),
+        compressQuality: 70,
+      );
+      return croppedFile;
+    }
+    return null;
+  }
+}
