4b1ec14ab13d1ae286aba94effcea6c136d00b7e
dadzi <dadziemikke2@gmail.com>
2021-11-19 10:42:15 +0000

    Added base post repository

---
+++ lib/features/create/data/repositories/base_post_repository.dart
@@ -0,0 +1,9 @@
+import 'package:socially/features/create/data/models/comment_model.dart';
+import 'package:socially/features/create/data/models/post_model.dart';
+
+abstract class BasePostRepository {
+  Future<void> createPost({required Post post});
+  Future<void> createComment({required Comment comment});
+  Stream<List<Future<Post>>> getUserPosts({required String userId});
+  Stream<List<Future<Comment>>> getPostomments({required String postId});
+}
