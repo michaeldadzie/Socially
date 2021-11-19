c6c44f6860d6f0d8b7dbb09b31e8eca6858eb444
dadzi <dadziemikke2@gmail.com>
2021-11-19 10:23:01 +0000

    Added new top collections and subcollection

--- lib/features/authentication/data/config/paths.dart
+++ lib/features/authentication/data/config/paths.dart
@@ -1,3 +1,9 @@
 class Paths {
+  // top level collection
   static const String users = 'users';
+  static const String posts = 'posts';
+  static const String comments = 'comments';
+
+  // subcollections
+  static const String postComments = 'postComments';
 }
