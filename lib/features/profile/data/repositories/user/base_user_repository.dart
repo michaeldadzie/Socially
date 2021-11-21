444f46010c3dad7f3024f84565f0f0c79ef77fca
dadzi <dadziemikke2@gmail.com>
2021-11-21 11:02:20 +0000

    Added method searchUsers to base user repository

--- lib/features/profile/data/repositories/user/base_user_repository.dart
+++ lib/features/profile/data/repositories/user/base_user_repository.dart
@@ -3,4 +3,5 @@ import 'package:socially/features/profile/data/models/user_model.dart';
 abstract class BaseUserRepository {
   Future<User> getUserWithId({required String userId});
   Future<void> updateUser({required User user});
+  Future<List<User>> searchUsers({required String query});
 }
