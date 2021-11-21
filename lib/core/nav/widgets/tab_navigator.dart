d0270617ae9c59a5f019226db2cd53253bec366a
dadzi <dadziemikke2@gmail.com>
2021-11-21 11:14:03 +0000

    Wrapped SearchScreen with a BlocProvider

--- lib/core/nav/widgets/tab_navigator.dart
+++ lib/core/nav/widgets/tab_navigator.dart
@@ -3,12 +3,16 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:socially/core/config/custom_router.dart';
 import 'package:socially/core/nav/enums/bottom_nav_item.dart';
 import 'package:socially/features/authentication/presentation/bloc/auth/auth_bloc.dart';
+import 'package:socially/features/create/data/repositories/post_repository.dart';
+import 'package:socially/features/create/presentation/cubit/create_post_cubit.dart';
 import 'package:socially/features/create/presentation/pages/create_screen.dart';
 import 'package:socially/features/feed/presentation/pages/feed_screen.dart';
 import 'package:socially/features/notifications/presentation/pages/notifications_screen.dart';
+import 'package:socially/features/profile/data/repositories/storage/storage_repository.dart';
 import 'package:socially/features/profile/data/repositories/user/user_repository.dart';
 import 'package:socially/features/profile/presentation/bloc/profile_bloc.dart';
 import 'package:socially/features/profile/presentation/pages/profile_screen.dart';
+import 'package:socially/features/search/presentation/cubit/search_cubit.dart';
 import 'package:socially/features/search/presentation/pages/search_screen.dart';
 
 class TabNavigator extends StatelessWidget {
@@ -49,9 +53,21 @@ class TabNavigator extends StatelessWidget {
       case BottomNavItem.feed:
         return FeedScreen();
       case BottomNavItem.search:
-        return SearchScreen();
+        return BlocProvider<SearchCubit>(
+          create: (context) => SearchCubit(
+            userRepository: context.read<UserRepository>(),
+          ),
+          child: SearchScreen(),
+        );
       case BottomNavItem.create:
-        return CreateScreen();
+        return BlocProvider<CreatePostCubit>(
+          create: (context) => CreatePostCubit(
+            postRepository: context.read<PostRepository>(),
+            storageRepository: context.read<StorageRepository>(),
+            authBloc: context.read<AuthBloc>(),
+          ),
+          child: CreateScreen(),
+        );
       case BottomNavItem.notifications:
         return NotificationsScreen();
       case BottomNavItem.profile:
@@ -59,6 +75,7 @@ class TabNavigator extends StatelessWidget {
           create: (_) => ProfileBloc(
             authBloc: context.read<AuthBloc>(),
             userRepository: context.read<UserRepository>(),
+            postRepository: context.read<PostRepository>(),
           )..add(ProfileLoadUser(
               userId: context.read<AuthBloc>().state.user!.uid)),
           child: ProfileScreen(),
