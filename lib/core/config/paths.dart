2ea33085193a9294ece456a96c8b467a26fcc16f
dadzi <dadziemikke2@gmail.com>
2021-11-28 20:26:50 +0000

    Moved paths.dart to core config

---
+++ lib/core/config/paths.dart
@@ -0,0 +1,15 @@
+class Paths {
+  // top level collection
+  static const String users = 'users';
+  static const String posts = 'posts';
+  static const String comments = 'comments';
+  static const String followers = 'followers';
+  static const String following = 'following';
+  static const String feeds = 'feeds';
+
+  // subcollections
+  static const String postComments = 'postComments';
+  static const String userFollowers = 'userFollowers';
+  static const String userFollowing = 'userFollowing';
+  static const String userFeed = 'userFeed';
+}
