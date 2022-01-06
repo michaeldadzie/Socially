19c0954d02e031dcd43d261c6e3d9cd3925d7f7e
dadzi <dadziemikke2@gmail.com>
2022-01-06 10:01:10 +0000

    Added getLikedPostIds, createLike and deleteLike methods

--- lib/features/create/data/repositories/base_post_repository.dart
+++ lib/features/create/data/repositories/base_post_repository.dart
@@ -4,8 +4,11 @@ import 'package:socially/features/create/data/models/post_model.dart';
 abstract class BasePostRepository {
   Future<void> createPost({required Post post});
   Future<void> createComment({required Comment comment});
+  void createLike({required Post post, required String userId});
   Stream<List<Future<Post>>> getUserPosts({required String userId});
   Stream<List<Future<Comment>>> getPostComments({required String postId});
   Future<List<Post>> getUserFeed(
       {required String userId, required String lastPostId});
+  Future<Set<String>> getLikedPostIds({required String userId, required List<Post> posts});
+  void deleteLike({required String postId, required String userId});
 }
